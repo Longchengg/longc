1eb33372b5f58e71c842c8e458156c97bf69d9fa
Longchengg <13452969830@163.com>
2015-11-19 14:58:36 +0800

    添加了文件
    的萨芬撒地方

---
+++ config.lua
@@ -0,0 +1,42 @@
+
+-- 0 - disable debug info, 1 - less debug info, 2 - verbose debug info
+DEBUG = 1
+
+-- display FPS stats on screen
+DEBUG_FPS = true
+
+-- dump memory info every 10 seconds
+DEBUG_MEM = false
+
+-- load deprecated API
+LOAD_DEPRECATED_API = false
+
+-- load shortcodes API
+LOAD_SHORTCODES_API = true
+
+-- screen orientation
+CONFIG_SCREEN_ORIENTATION = "landscape"
+
+-- design resolution
+CONFIG_SCREEN_WIDTH  = 960
+CONFIG_SCREEN_HEIGHT = 640
+
+-- auto scale mode
+CONFIG_SCREEN_AUTOSCALE = "FIXED_HEIGHT"
+
+-- 汉字图片
+TEXTURE_CHINESERESOURSE_DATA  = "chineseresources.plist"
+TEXTURE_CHINESERESOURSE_IMAGE = "chineseresources.png"
+
+-- 按钮图片
+TEXTURE_COVER_DATA  = "Cover/Cover0.plist"
+TEXTURE_COVER_IMAGE = "Cover/Cover0.png"
+
+-- MainMenu图片
+TEXTURE_MAINMENU_DATA  = "MainMenu/MainMenu0.plist"
+TEXTURE_MAINMENU_IMAGE = "MainMenu/MainMenu0.png"
+
+-- 音乐文件
+GAME_SFX = {
+    loseMusic      = "Audio/ui/lose.ogg",
+}
