28756aae2dc1077f19f27c87cb3f17a5cf7cdca6
Longchengg <13452969830@163.com>
2015-11-19 15:11:13 +0800

    Revert "再次加入"
    This reverts commit 7db9a247a2fe7e9cb40ce1d6132fbd2466219fb1.

--- ClassManager.lua
+++
@@ -1,9 +0,0 @@
---
--- Author: zyuq
--- Date: 2015-11-12 09:29:41
---
-SetLayer =  require("app.Class.SetLayer")
-Music = require("app.Class.Music")
-OperateLayer = require("app.Class.OperateLayer")
-HRocker = require("app.Class.HRocker")
-Hero = require("app.Class.Hero")
